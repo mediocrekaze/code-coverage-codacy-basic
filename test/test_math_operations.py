7b06fec4a2ce2c95d0a794ce387acf6f69b636bc
Allan Arceno <arcenoallan214@gmail.com>
2025-02-24 23:49:11 +0800

    2025-02-24: base code commit

---
+++ test/test_math_operations.py
@@ -0,0 +1,12 @@
+import unittest
+from app.math_operations import add, subtract, multiply
+
+class TestMathOperations(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(2, 3), 6)
